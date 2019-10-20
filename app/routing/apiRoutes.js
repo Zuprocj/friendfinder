0f946797229f4f6fea2e320c0f41a204a4fe24a3
Zuprocj <50551420+Zuprocj@users.noreply.github.com>
2019-10-20 09:55:31 -0700

    update apiroutes

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,11 @@
+var friendsData = require('../data/friendsData')
+
+module.exports = (function(app) {
+
+app.get('/api/friends', function(req,res) {
+res.join(friends);
+
+app.post('/api/friends, function(req,res) {
+var newFriend = req.body;
+friendlist.push(newFriend);
+});
