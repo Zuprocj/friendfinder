e647be60bd46b92a55f4857ff7fcadcde5d6bbf8
Zuprocj <50551420+Zuprocj@users.noreply.github.com>
2019-10-20 12:48:41 -0700

    add GET route and new variables

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,10 +1,23 @@
-var friendsData = require('../data/friendsData')
+var friends = require('../data/friendsData')
 
 module.exports = (function(app) {
 
 app.get('/api/friends', function(req,res) {
-res.join(friends);
-
+  res.join(friends);
+});
+  var bestMatch = {
+    name: '',
+    photo:'',
+    friendDifference: 1000
+  };
+  
+  console.log(req.body);
+  
+  var userData = req.body;
+  var userScores = userData.scores;
+  
+  console.log(userScores);
+  
 app.post('/api/friends, function(req,res) {
 var newFriend = req.body;
 friendlist.push(newFriend);
