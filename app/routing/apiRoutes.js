0a043e242a8df4d19fba171626dd24e3f68c72d2
zuprocj <jmdcorpuz@gmail.com>
2019-10-23 16:18:43 -0700

    debugging

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,30 +7,34 @@ module.exports = function(app) {
   });
   
   app.post('/api/friends', function(req, res) {
-  
+    
   var bestMatch = {
     name: '',
     photo: '',
     friendDifference: 1000
   };
+  
   var userData = req.body;
-  var userScores = userData.scores;  
-
-  var totalDifference = 0;
-
+  var userScores = userData.scores;
+  
+  var totalDifference =  0;
+  
     for (var i = 0; i < friends.length; i++) {
       totalDifference = 0;
+
       for (var j = 0; j < friends[i].scores[j]; j++) {
         totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
-          if (totalDiffernece <= bestMatch.friendDifference) {
+
+          if (totalDifference <= bestMatch.friendDifference) {
           bestMatch.name = friends[i].name;
           bestMatch.photo = friends[i].photo;
           bestMatch.friendDifference = totalDifference;
         }
       }
     }
-
+    
   friends.push(userData);  
   res.json(bestMatch);
+  
   });
 };
--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 var express = require('express');
+var app = express();
 var bodyParser = require('body-parser')
 var path = require('path');
-
-var app = express();
-
 var PORT = process.env.PORT || 8080
 
 app.use(bodyParser.json());
