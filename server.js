f7fd8629008b00c923e664f4bd0c9325f033716d
zuprocj <jmdcorpuz@gmail.com>
2019-10-23 22:35:30 -0700

    require routes

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
-require('./app/routing/apiRoutes.js')(app);
-require('./app/routing/htmlRoutes.js')(app);
+require('./app/routing/api-routes.js')(app);
+require('./app/routing/html-routes.js')(app);
 
 app.listen(PORT, function() {
     console.log("Server listening on: http://localhost:" + PORT);
