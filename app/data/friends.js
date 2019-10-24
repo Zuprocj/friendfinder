bb99d024009bdb4ec7a479cfd8f1b1850e158970
zuprocj <jmdcorpuz@gmail.com>
2019-10-23 23:25:34 -0700

    path npm not working trying to debug

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,64 +1,64 @@
 var friends = [
   {
-  'name': 'Angela',
-  'photo': 'https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters_opt/p-the-office-angela-kinsey.jpg',
-  'scores': [2,1,3,5,4,1,5,5,4,5]
+  name: 'Angela',
+  photo: 'https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters_opt/p-the-office-angela-kinsey.jpg',
+  scores: [2,1,3,5,4,1,5,5,4,5]
   },
   {
-  'name': 'Creed',
-  'photo': 'https://i.ytimg.com/vi/mSSRTGuFH6o/maxresdefault.jpg',
-  'scores': [1,1,2,2,2,1,2,4,2,2]
+  name: 'Creed',
+  photo: 'https://i.ytimg.com/vi/mSSRTGuFH6o/maxresdefault.jpg',
+  scores: [1,1,2,2,2,1,2,4,2,2]
   }, 
   {
-  'name': 'Dwight',
-  'photo': 'https://upload.wikimedia.org/wikipedia/en/c/cd/Dwight_Schrute.jpg',
-  'scores': [5,4,4,4,5,4,4,5,4,3]
+  name: 'Dwight',
+  photo: 'https://upload.wikimedia.org/wikipedia/en/c/cd/Dwight_Schrute.jpg',
+  scores: [5,4,4,4,5,4,4,5,4,3]
   },
   {
-  'name': 'Jim',
-  'photo': 'https://miro.medium.com/max/1569/0*Xz-_cHSO6txphvHt.png',
-  'scores': [1,2,3,3,4,4,3,3,5,5]
+  name: 'Jim',
+  photo: 'https://miro.medium.com/max/1569/0*Xz-_cHSO6txphvHt.png',
+  scores: [1,2,3,3,4,4,3,3,5,5]
   },
   {
-  'name': 'Meredith',
-  'photo': 'https://pbs.twimg.com/profile_images/690396785270001667/J3Sbdo8n_400x400.jpg',
-  'scores': [5,2,4,4,5,3,2,4,3,2]
+  name: 'Meredith',
+  photo: 'https://pbs.twimg.com/profile_images/690396785270001667/J3Sbdo8n_400x400.jpg',
+  scores: [5,2,4,4,5,3,2,4,3,2]
   },
   {
-  'name': 'Michael',
-  'photo': 'https://upload.wikimedia.org/wikipedia/en/d/dc/MichaelScott.png',
-  'scores': [5,5,4,4,2,3,3,5,5,5]
+  name: 'Michael',
+  photo: 'https://upload.wikimedia.org/wikipedia/en/d/dc/MichaelScott.png',
+  scores: [5,5,4,4,2,3,3,5,5,5]
   },
   {
-  'name': 'Kevin',
-  'photo': 'https://pbs.twimg.com/profile_images/762958606199320576/WEa6ttXr_400x400.jpg',
-  'scores': [1,2,3,4,5,5,4,3,2,1]
+  name: 'Kevin',
+  photo: 'https://pbs.twimg.com/profile_images/762958606199320576/WEa6ttXr_400x400.jpg',
+  scores: [1,2,3,4,5,5,4,3,2,1]
   },
   {
-  'name': 'Oscar',
-  'photo': 'https://i.pinimg.com/originals/ed/73/b6/ed73b64b3a545549cfb3ead6f32388f8.jpg',
-  'scores': [5,5,3,3,5,1,2,3,2,5]
+  name: 'Oscar',
+  photo: 'https://i.pinimg.com/originals/ed/73/b6/ed73b64b3a545549cfb3ead6f32388f8.jpg',
+  scores: [5,5,3,3,5,1,2,3,2,5]
   },
   {
-  'name': 'Pam',
-  'photo': 'https://theofficeanalytics.files.wordpress.com/2017/09/pam-halpert-1.jpg?w=1024',
-  'scores': [4,3,4,4,5,3,4,3,4,1]
+  name: 'Pam',
+  photo: 'https://theofficeanalytics.files.wordpress.com/2017/09/pam-halpert-1.jpg?w=1024',
+  scores: [4,3,4,4,5,3,4,3,4,1]
   },
   {
-  'name': 'Phyllis',
-  'photo': 'https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters_opt/p-the-office-phyllis-smith.jpg',
-  'scores': [3,1,3,2,4,1,2,2,4,2]
+  name: 'Phyllis',
+  photo: 'https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters_opt/p-the-office-phyllis-smith.jpg',
+  scores: [3,1,3,2,4,1,2,2,4,2]
   },
   {
-  'name': 'Stanley',
-  'photo': 'https://wsbt.com/resources/media/a635a1b6-4cc4-425b-b686-67fa860f9ed8-large16x9_stanleyhudsonNBC.PNG?1550090897080',
-  'scores': [1,1,1,1,1,1,1,1,1,1]
+  name: 'Stanley',
+  photo: 'https://wsbt.com/resources/media/a635a1b6-4cc4-425b-b686-67fa860f9ed8-large16x9_stanleyhudsonNBC.PNG?1550090897080',
+  scores: [1,1,1,1,1,1,1,1,1,1]
   },
   {
-  'name': 'Toby',
-  'photo': 'https://i.redd.it/0er5ubrnsrh01.jpg',
-  'scores': [3,1,2,2,3,1,2,3,2,1]
+  name: 'Toby',
+  photo: 'https://i.redd.it/0er5ubrnsrh01.jpg',
+  scores: [3,1,2,2,3,1,2,3,2,1]
   }
-]
+];
 
 module.exports = friends;
--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,32 +1,30 @@
-var friends = require('../data/friends.js')
+var friends = require("../data/friends");
 
 module.exports = function(app) {
-
-  app.get('/api/friends', function(req,res) {
+  app.get("/api/friends", function(req, res) {
     res.json(friends);
   });
-  
-  app.post('/api/friends', function(req, res) {
-    console.log(req.body.scores)
 
-  var user = req.body;
-
-  for(var i = 0; i < user.scores.length; i++) {
-    user.scores[i] = parseInt(user.scores[i]);
-  }
-  
-  for(var i = 0; i < friends.length; i++) {
-    var totalDifference = 0;
-    for(var j = 0; j < friends[i].scores.length; j++) {
-      var difference = Math.abs(user.scores[j] - friends[i].scores[j]);
-      totalDifference += difference;
+  app.post("/api/friends", function(req, res) {
+    console.log(req.body.scores);
+    var user = req.body;
+    for(var i = 0; i < user.scores.length; i++) {
+      user.scores[i] = parseInt(user.scores[i]);
     }
-    if(totalDifference < minimumDifference) {
-      bestFriendIndex = i;
-      minimumDifference = totalDifference;
+    var bestFriendIndex = 0;
+    var minimumDifference = 40;
+    for(var i = 0; i < friends.length; i++) {
+      var totalDifference = 0;
+      for(var j = 0; j < friends[i].scores.length; j++) {
+        var difference = Math.abs(user.scores[j] - friends[i].scores[j]);
+        totalDifference += difference;
+      }
+      if(totalDifference < minimumDifference) {
+        bestFriendIndex = i;
+        minimumDifference = totalDifference;
+      }
     }
-  }
-  friends.push(user);
-  res.json(friends[bestFriendIndex]);
-});
+    friends.push(user);
+    res.json(friends[bestFriendIndex]);
+  });
 };
